b6ab12c934f4933390fac9074f5e136245fa2699
NamNPH <80657434+nphnamm@users.noreply.github.com>
2024-11-25 11:49:07 +0700

    add interview question

--- .NET/Microservice/Mango/Mango.Services.CouponAPI/obj/Debug/net8.0/Mango.Services.CouponAPI.AssemblyInfo.cs
+++ .NET/Microservice/Mango/Mango.Services.CouponAPI/obj/Debug/net8.0/Mango.Services.CouponAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4a2407a6e96790d8f0840a6430062be2c8c8fe0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+684b51f4547675fa216e075b78ecbac4884d252a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
